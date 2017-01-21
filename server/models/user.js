c79003e5367888d4f5a731ff6c7f67f5d9e628ae
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-01-21 18:38:51 -0300

    Agregar admin a user

--- server/models/user.js
+++ server/models/user.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 let userSchema = mongoose.Schema({
     username: String,
     password: String,
-    spcode: String // SimplePush key
+    spcode: String, // SimplePush key
+    admin: Boolean
 });
 
 let User = mongoose.model('User', userSchema);
