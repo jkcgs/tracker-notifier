b301c1596edc72f0df33907cec59845061f1329a
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-04-19 21:27:11 -0300

    Use request-promise-native

--- server/trackers/dhl.js
+++ server/trackers/dhl.js
@@ -1,4 +1,4 @@
-const request = require('request-promise');
+const request = require('request-promise-native');
 const cheerio = require('cheerio');
 const moment = require('moment');
 const base = require('../tracker-base');
