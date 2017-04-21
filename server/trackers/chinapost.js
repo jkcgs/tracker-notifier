e7f08a6b9459db7391be045c8b829325c8b9551d
Jonathan Gutiérrez <contacto@jgutierrez.cl>
2017-04-21 19:21:50 -0300

    Chinapost

--- server/trackers/chinapost.js
+++ server/trackers/chinapost.js
@@ -1,11 +1,10 @@
 const request = require('request-promise-native');
-//const cheerio = require('cheerio');
-//const moment = require('moment');
+const cheerio = require('cheerio');
+const moment = require('moment');
 const base = require('../tracker-base');
 const debug = require('debug')('tracker-notifier:chinapost');
 module.exports = ChinaPost;
 
-// TODO: Stub
 function ChinaPost() {
     this.name = 'ChinaPost';
     this.getStatus = getStatus;
@@ -17,10 +16,18 @@ function ChinaPost() {
                 throw new Error(base.errors.notExists);
             }
 
+            let $ = cheerio.load(data);
+            let table = $('table').eq(2);
+            let statuses = table.find('tr').eq(0);
+
+            let time = statuses.eq(0).find('td').eq(0).text();
+            let status = statuses.eq(0).find('td').eq(1).text();
+            let ptime = moment(time, 'YYYY-MM-DD HH:mm:ss.0');
+
             return {
                 code: code,
-                status: 'Unknown',
-                date: new Date(0),
+                status: status,
+                date: ptime.toDate(),
                 location: 'Unknown',
                 delivered: false,
                 deliveryInfo: {}
